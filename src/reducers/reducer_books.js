d1a93beff7176eb902c60bb809df1762d75dbbb1
Jonathan Torres <Jonathan.torres25@live.com>
2018-04-08 23:02:51 -0400

    deploying app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: "harry potter", pages: 101},
+    {title: "Javascript: the Good parts" , pages: 300},
+    {title: "the dark tower", pages: 40},
+    {title: "hello may", pages: 200},
+    {title: "halo", pages: 100}
+  ]
+}
